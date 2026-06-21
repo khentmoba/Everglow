422df63a158f1c4a8d0f56f330b814c41f9cbf4f
khentmoba <khentplaysmoba@gmail.com>
2026-06-22 04:42:21 +0800

    fix: remove service worker reload mechanism entirely
    Replace custom SW with self-unregistering no-op to eliminate
    the infinite reload loop. Remove SW registration from index.html
    so no new service worker is installed.

--- web/sw.js
+++ web/sw.js
@@ -1,29 +1,7 @@
-const BUILD = '5.3.0+1-61d22f9';
-let isUpdate = false;
-
-self.addEventListener('install', (event) => {
-  isUpdate = !!self.registration.active;
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
-    }).then(() => {
-      return self.clients.claim();
-    }).then(() => {
-      if (isUpdate) {
-        return self.clients.matchAll().then((clients) => {
-          clients.forEach((client) => {
-            client.postMessage({ type: 'NEW_VERSION', version: '5.3.0+1-61d22f9' });
-          });
-        });
-      }
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', () => {
+  self.registration.unregister();
 });
