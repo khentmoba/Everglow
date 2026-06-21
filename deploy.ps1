213b746f211b8a72912bdd9005c42d94a77e83b7
khentmoba <khentplaysmoba@gmail.com>
2026-06-22 04:42:21 +0800

    fix: remove service worker reload mechanism entirely
    Replace custom SW with self-unregistering no-op to eliminate
    the infinite reload loop. Remove SW registration from index.html
    so no new service worker is installed.

--- deploy.ps1
+++ deploy.ps1
@@ -8,34 +8,12 @@ if (-not $commitHash) { $commitHash = "unknown" }
 $buildConst = "$version-$commitHash"
 
 $swContent = @"
-const BUILD = '$buildConst';
-let isUpdate = false;
-
-self.addEventListener('install', (event) => {
-  isUpdate = !!self.registration.active;
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
-    }).then(() => {
-      return self.clients.claim();
-    }).then(() => {
-      if (isUpdate) {
-        return self.clients.matchAll().then((clients) => {
-          clients.forEach((client) => {
-            client.postMessage({ type: 'NEW_VERSION', version: '$buildConst' });
-          });
-        });
-      }
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', () => {
+  self.registration.unregister();
 });
 "@
 
