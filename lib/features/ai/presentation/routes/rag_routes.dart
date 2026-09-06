a1c9d9fb4bb72526bd7a7af06a187c8b42621760
Khent Felary G Sanco <269314448+khentmoba@users.noreply.github.com>
2026-09-06 21:48:35 +0800

    chore(dashboard): remove cookbook, atlas, vault, wellness, universe, budget, ask

--- lib/features/ai/presentation/routes/rag_routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:go_router/go_router.dart';
-import '../screens/rag_screen.dart';
-
-final List<GoRoute> ragRoutes = [
-  GoRoute(path: '/rag', builder: (_, _) => const RagScreen()),
-];
